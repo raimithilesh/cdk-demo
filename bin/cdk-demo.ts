87933bc381be6c09f4c8f419f2a3980e887c8ad7
Rai <mithilesh.rai@capgemini.com>
2022-04-27 17:04:53 +0100

    Import stack file

--- bin/cdk-demo.ts
+++ bin/cdk-demo.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-//import { DevopsDemoStack } from '../lib/devops-demo-stack';
+import { CDKDemoStack } from '../lib/cdk-demo-stack';
 
 const app = new cdk.App();
 new CDKDemoStack(app, 'CDKDemoStack', {
