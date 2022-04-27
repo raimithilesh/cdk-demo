a0bad5cadc849e76be0c54d775ebceb285d49452
raimithilesh <59111274+raimithilesh@users.noreply.github.com>
2022-04-27 17:43:29 +0100

    Update handler.ts

--- lib/lambda/handler.ts
+++ lib/lambda/handler.ts
@@ -2,7 +2,7 @@
 export async function handler(event: string, context: string) {
   console.log('Stage Name is: ' + process.env.stage);
   return {
-    body: 'Hello from a Lambda Function',
+    body: 'Hello Devops from a Lambda Function',
     statusCode: 200,
   };
 }
