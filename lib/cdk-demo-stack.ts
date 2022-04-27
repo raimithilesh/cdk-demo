8aea129b1765e655bd1be9f25fae3698d5b0e9a7
Rai <mithilesh.rai@capgemini.com>
2022-04-27 17:16:35 +0100

    Add  more stages

--- lib/cdk-demo-stack.ts
+++ lib/cdk-demo-stack.ts
@@ -1,13 +1,13 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { CodePipeline, CodePipelineSource, ManualApprovalStep, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
-//import { MyPipelineAppStage } from './stage';
+import { MyPipelineAppStage } from './stage';
 
 export class CDKDemoStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new CodePipeline(this, 'Pipeline', {
+    const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'TestPipeline1',
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('raimithilesh/cdk-demo', 'main'),
@@ -17,5 +17,16 @@ export class CDKDemoStack extends Stack {
       })
     });
 
+    const testingStage = pipeline.addStage(new MyPipelineAppStage(this, "test", {
+          env: { account: "713411715170", region: "us-east-1" }
+        }));
+
+
+        testingStage.addPre(new ShellStep("Run Unit Tests", { commands: ['npm install', 'npm test'] }));
+        testingStage.addPost(new ManualApprovalStep('Manual approval before production'));
+
+        const prodStage = pipeline.addStage(new MyPipelineAppStage(this, "prod", {
+          env: { account: "713411715170", region: "us-east-1" }
+        }));
   }
 }
